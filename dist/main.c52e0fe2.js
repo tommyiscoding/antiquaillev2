ec4eddfbc452caf16f002815164a0a9c715fa8de
tommy <thomas.freger@gmail.com>
2020-12-08 12:46:40 +0100

    re GDPR en

--- dist/main.c52e0fe2.js
+++ dist/main.c52e0fe2.js
@@ -150,7 +150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8035" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12989" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
