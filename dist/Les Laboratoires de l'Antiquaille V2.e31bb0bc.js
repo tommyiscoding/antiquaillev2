0e9af5179bb6afe58bc57f9058daa4e46625ebdc
THMS <thms.studio@gmail.com>
2022-05-25 14:09:55 +0200

    corrections suite update

--- dist/Les Laboratoires de l'Antiquaille V2.e31bb0bc.js
+++ dist/Les Laboratoires de l'Antiquaille V2.e31bb0bc.js
@@ -11188,7 +11188,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "2875" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "24058" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
